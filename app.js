d2b94a7f967ef19bdadfe51c0ecb1e88e37067ba
SlickJohnson <liwa.johnson@gmail.com>
2018-08-29 22:28:06 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express();
 
 var exphbs = require('express-handlebars');
 
+let reviews = [
+  { title: "Greate Review" },
+  { title: "Next Review" },
+]
+
+app.get('/reviews', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
+});
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 });
 
 app.listen(3000, () => {
