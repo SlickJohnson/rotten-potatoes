f099b7c77e2ac4e055a7ba5fbc506c775cd7d935
SlickJohnson <liwa.johnson@gmail.com>
2018-09-15 17:14:04 -0700

    Add show movies route

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,31 @@
+const MovieDb = require('moviedb-promise');
+const moviedb = new MovieDb('fe65cf132786c888aae3b54309b7369d');
+
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.render('movies-index');
+    moviedb.miscNowPlayingMovies().then((response) => {
+      res.render('movies-index', { movies: response.results });
+    }).catch((err) => {
+      console.log(err.message);
+    });
+  });
+
+  app.get('/movies/:id', (req, res) => {
+    moviedb.movieInfo({ id: req.params.id }).then((movie) => {
+      if (movie.video) {
+        moviedb.movieVides({ id: req.params.id }).then((videos) => {
+          movie.trailer_youtube_id = videos.results[0].key;
+          renderTemplate(movie);
+        });
+      } else {
+        renderTemplate(movie);
+      }
+
+      function renderTemplate(movie) {
+        res.render('movies-show', { movie });
+      }
+    }).catch((err) => {
+      console.log(err.message);
+    });
   });
 };
